f8cdee26b1ab50e83bf7c80d11606d65511f4b59
TsyShin <545656660@qq.com>
2018-12-09 15:22:55 +0800

    '项目初始化完成'

---
+++ src/pages/cart/index.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>这是购物车页面
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+    }
+  },
+  methods: {
+  }
+};
+</script>
+
+<style scoped>
+</style>
---
+++ src/pages/category/index.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>这是分类页
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+    }
+  },
+  methods: {
+  }
+};
+</script>
+
+<style scoped>
+</style>
